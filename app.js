64b56eff5111ee878dc6026a118a0d40c42e5e21
ms18426 <muhammad.shabbir@stud.th-deg.de>
2022-09-30 14:46:30 +0200

    updated code

--- app.js
+++ app.js
@@ -131,6 +131,73 @@ app.delete('/tasklists/:tasklistId', (req, res) => {
         .catch((err) => { console.log(err) })
 })
 
+
+
+
+
+/* RESTFUL implimentation of Task 
+
+Every tasklist will have its own tasks
+so the url will smt like http://localhost:3000/tasklists/:tasklistId/task:taskid
+
+*/
+
+//Get all task related to a certain tasklist id
+
+app.get('/tasklists/:tasklistId/task', (req, res) => {
+    Task.find({ _tasklistId: req.params.tasklistId })
+        .then((task) => { res.send(task) })
+        .catch((err) => { console.log(err) })
+})
+
+
+app.post('/tasklists/:tasklistId/task', (req, res) => {
+    let taskobj = { 'title': req.body.title, '_tasklistId': req.params.tasklistId };
+    Task(taskobj).save()
+        .then((taskpost) => {
+            res.status(201);
+            res.send(taskpost);
+
+        })
+        .catch((err) => { console.log(err) })
+})
+
+
+
+
+/* Get One Task */
+app.get('/tasklists/:tasklistId/task/:taskId', (req, res) => {
+    Task.findOne({ _tasklistId: req.params.tasklistId, _id: req.params.taskId })
+        .then((task) => { res.send(task) })
+        .catch((err) => { console.log(err) })
+})
+
+
+/* update Task */
+
+app.patch('/tasklists/:tasklistId/task/:taskId', (req, res) => {
+    Task.findByIdAndUpdate({ _tasklistId: req.params.tasklistId, _id: req.params.taskId }, { $set: req.body })
+        .then((taskupdate) => {
+            res.status(201);
+            res.send(taskupdate);
+
+        })
+        .catch((err) => { console.log(err) })
+})
+
+
+/* Delete */
+
+app.delete('/tasklists/:tasklistId/task/:taskId', (req, res) => {
+    Task.findOneAndDelete({ _tasklistId: req.params.tasklistId, _id: req.params.taskId })
+        .then((taskupdate) => {
+            res.status(201);
+            res.send(taskupdate);
+
+        })
+        .catch((err) => { console.log(err) })
+})
+
 app.listen(3000, () => {
     console.log("Server Started on port 3000");
 });
